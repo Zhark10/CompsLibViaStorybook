c82baa06dfee63d3f2f5d2a6828975a4f5d77c0f
Zhark10 <verygoodman18@yandex.ru>
2020-10-13 17:40:38 +0300

    button presentation created

---
+++ packages/universal/src/components/Buttons/lib/ButtonComponent.stories.d.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=ButtonComponent.stories.d.js.map
--- packages/universal/universal.ts
+++ packages/universal/universal.ts
@@ -1,3 +1,2 @@
 export { default as ButtonComponent } from './src/components/Buttons';
-export { default as IconComponent } from './src/components/Icons';
 export { default as TextComponent } from './src/components/Text';
